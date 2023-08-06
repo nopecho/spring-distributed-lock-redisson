24a45dda884c4fedfe6048d09138992ec804d845
nopecho <ch.jooon@gmail.com>
2023-08-06 20:33:08 +0900

    feat: 애노테이션 설정값 추가

--- core/aop/src/main/java/io/nopecho/distributed/DistributedLock.java
+++ core/aop/src/main/java/io/nopecho/distributed/DistributedLock.java
@@ -1,4 +1,25 @@
 package io.nopecho.distributed;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This Is An Annotation For The Implementation Of Distributed Locks.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
+    /**
+     * @return 락 획득 시 사용될 key 값
+     */
+    String key();
+
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    long waitTime() default 3L;
+
+    long maximumLockTime() default 3L;
 }
